2a7b4ad36a7c5ce211a6179b87238f73dcaac55e
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-01 23:50:44 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,9 +12,9 @@ class AuthorsController < ApplicationController
     
     if @author.valid?
       @author.save 
-      redirect_to new_author_path 
-    else 
       redirect_to author_path(@author)
+    else 
+      render :new 
     end 
     
   end
